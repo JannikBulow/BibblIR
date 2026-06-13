2a1f9f94be467b8b6735314abf63741d0c3944d7
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-06-13 16:40:27 +0200

    feat: add return instruction

---
+++ framework/include/BibblIR/ir/instruction/return_instruction.h
@@ -0,0 +1,26 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#ifndef BIBBLIR_IR_INSTRUCTION_RETURN_INSTRUCTION_H
+#define BIBBLIR_IR_INSTRUCTION_RRETURN_INSTRUCTION_H
+
+#include "BibblIR/ir/instruction.h"
+
+namespace bibblir {
+    class ReturnInstruction : public Instruction {
+        friend class IRBuilder;
+        friend class PrintVisitor;
+    public:
+        std::vector<std::reference_wrapper<Value*>> getOperands() override;
+
+        std::string identifier() const override;
+
+        void accept(Visitor& visitor) override;
+
+    private:
+        ReturnInstruction(BasicBlock* parent, Value* returnValue);
+
+        Value* mReturnValue;
+    };
+}
+
+#endif //BIBBLIR_IR_INSTRUCTION_RRETURN_INSTRUCTION_H
---
+++ framework/src/ir/instruction/return_instruction.cpp
@@ -0,0 +1,37 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#include "BibblIR/ir/instruction/return_instruction.h"
+
+#include "BibblIR/ir/basicblock.h"
+#include "BibblIR/ir/function.h"
+
+#include "BibblIR/visitor/visitor.h"
+
+#include <cassert>
+
+namespace bibblir {
+    std::vector<std::reference_wrapper<Value*>> ReturnInstruction::getOperands() {
+        if (mReturnValue) return {mReturnValue};
+        else return {};
+    }
+
+    std::string ReturnInstruction::identifier() const {
+        return "%undef";
+    }
+
+    void ReturnInstruction::accept(Visitor& visitor) {
+        visitor.visit(*this);
+    }
+
+    ReturnInstruction::ReturnInstruction(BasicBlock* parent, Value* returnValue)
+        : Instruction(parent->getModule(), parent)
+        , mReturnValue(returnValue) {
+        mRequiresRegister = true;
+
+        if (!mReturnValue) {
+            assert(mParent->getParent()->getFunctionType()->getReturnType()->isVoidType());
+        } else {
+            assert(mReturnValue->getType() == mParent->getParent()->getFunctionType()->getReturnType());
+        }
+    }
+}
